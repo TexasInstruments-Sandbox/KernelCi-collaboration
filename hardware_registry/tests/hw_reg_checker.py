fd434d140c28cc27389463e27e7bb160f9861da0
Minas Hambardzumyan <minas@ti.com>
2025-10-06 17:16:20 -0500

    hardware_registry/tests/hw_reg_checker.py: hw registry test script
    Added a script to test the hardware registry against the schema
    
    Signed-off-by: Minas Hambardzumyan <minas@ti.com>

---
+++ hardware_registry/tests/hw_reg_checker.py
@@ -0,0 +1,21 @@
+import yaml
+from jsonschema import validate, ValidationError
+
+# Load schema
+schema = yaml.safe_load(open('../hardware_registry_schema.yaml'))
+data_dict  = yaml.safe_load(open('../hardware_registry.yaml'))
+
+try:
+    validate(instance=data_dict, schema=schema)
+    print("? Validation successful � corrected dataset conforms to schema!")
+    print(f"Silicon vendors: {len(data_dict['silicon_vendors'])}")
+    print(f"Processors: {len(data_dict['processors'])}")
+    print(f"Board vendors: {len(data_dict['board_vendors'])}")
+    if 'system_modules' in data_dict:
+        print(f"System modules: {len(data_dict['system_modules'])}")
+    print(f"Boards: {len(data_dict['boards'])}")
+except ValidationError as e:
+    print("? Validation error:")
+    print(f"   Message: {e.message}")
+    print(f"   Path: {' -> '.join(map(str,e.path))}")
+    print(f"   Schema path: {' -> '.join(map(str,e.schema_path))}")
